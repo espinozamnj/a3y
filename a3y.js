0b948e49e35b735108262f46cec3cbc68f15cf78
espinozamnj <1025179@senati.pe>
2022-12-31 14:21:46 -0500

    all

---
+++ a3y.js
@@ -0,0 +1,141 @@
+document.title = '[Tab]'
+var sv
+window.addEventListener('load', function() {
+    sett.attr.split('||').forEach(function(a){
+        let r = a.split('|')
+        $('#val').setAttribute(r[0], r[1])
+    })
+    var lastIframe = ''
+    var pattern = new RegExp('^(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/.*)?$', 'i')
+    // console.log(pattern)
+    function validURL(str) {
+        // pattern = new RegExp('^(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%@_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$', 'i')
+        return !!pattern.test(str.toLowerCase())
+    }
+    $('#pag').src = _md + '_j.em/links?!'
+    function resA() {
+        $('#app').src = 'apps.html'
+    }
+    resA()
+    sett.ascii.split('@').forEach(function(c){
+        let co = document.createElement('code')
+        co.innerText = c
+        $('#aci').appendChild(co)
+    })
+    $('#aci').setAttribute('href', location.href)
+    $('#aci').setAttribute('target', '_blank')
+    let gc = document.getElementsByTagName('code'),
+        li = 0,
+        s = 180
+    while (li < gc.length) {
+        let c = (s += 10)
+        gc[li].style.color = 'rgb(' + c + ',' + c + ',' + c + ')'
+        li += 1
+    }
+    // let i = $('#val')
+    let i = $('#goo')
+    sv = function(val) {
+        let r = decodeURIComponent(val)
+        function on(t) {
+            t.endsWith('*') ? open(t.slice(0, -1), '', 'noreferrer') : open(t, '_top', 'noreferrer')
+        }
+        if (val.startsWith('?')) {
+            $('#app').src ='https://www.google.com/search?q=' + encodeURIComponent(val.slice(1)) + '&igu=1'
+            $('#apps').click()
+        } else if (val.startsWith('--')) {
+            $('#pag').src = _md + '_j.em/links?' + encodeURIComponent(val.slice(2))
+            $('.clock').click()
+        } else if (val.startsWith('-')) {
+            on(_md + '_j.em/links?' + encodeURIComponent(val.slice(1)))
+        } else if (val.startsWith('..')) {
+            on(`https://ddg.co/?q=${(r.slice(2))}`)
+        } else if (val.startsWith('.')) {
+            on(`https://ddg.co/?q=!${(r.slice(1))}`)
+        } else if (val.startsWith('save:')) {
+            on(`${_md}_j.em/off/firenote?autosave=${(encodeURIComponent(r.split('save:')[1]))}`)
+        } else if (val.startsWith(':')) {
+            if ($('.over')) {
+                $('.over').click()
+            }
+        } else if (validURL(val.replace(/\*$/g,''))) {
+            val.startsWith('http') ? on(val) : on('//' + val)
+        } else {
+            on(`https://goo.gl/search/${(r)}`)
+        }
+    }
+    $('#send').addEventListener('click', function () {
+        sv(i.value)
+    })
+    $('#lupa').addEventListener('click', function () {
+        sv(i.value)
+    })
+    document.addEventListener('keydown', function (ev) {
+        if (ev.shiftKey && ev.code == 'Space') {
+            i.click()
+            i.focus()
+            setTimeout(function () {
+                i.value = ''
+            }, 8e2)
+        } else if (ev.ctrlKey && ev.code == 'Space') {
+            $('.clock').click()
+        } else if ((ev.code == 'KeyA' || ev.which == 65) && ev.shiftKey) {
+            embed('app')
+        } else {}
+    })
+    i.addEventListener('keydown', function (e) {
+        let ts = e.target.value
+        if ((e.code == 'Enter' || e.which == 13) && !e.ctrlKey) {
+            sv(ts)
+        }
+        if ((e.code == 'Enter' || e.which == 13) && e.ctrlKey) {
+            ts.endsWith('*') ? sv(ts) : sv(ts + '*')
+        }
+    })
+    function embed(id) {
+        let s = lastIframe == id ? true : false
+        lastIframe = id
+        let a = $('.app'), sta = 'pan'
+        if (a.className.includes(sta) && s) {
+            a.classList.remove(sta)
+        } else {
+            a.classList.add(sta)
+        }
+        $('.panel').childNodes.forEach(function(f){
+            if (f.nodeType !== 3) {
+                if (f.id == id) {
+                    f.style.display = ''
+                    f.focus()
+                } else {
+                    f.style.display = 'none'
+                }
+            }
+        })
+
+    }
+    $('.block').addEventListener('click', function () {
+        $('.app').classList.replace('u', 'b')
+    })
+    $('.unblock').addEventListener('click', function () {
+        $('.app').classList.replace('b', 'u')
+    })
+    $('.clock').addEventListener('click', function () {
+        embed('pag')
+    })
+    $('#apps').addEventListener('click', function () {
+        embed('app')
+    })
+    $('#apps').addEventListener('dblclick', function () {
+        resA()
+    })
+    g_S.length > 0 && ($('.clock').click())
+    clockA3Y(document.getElementById('.clock'), 1e4)
+})
+window.addEventListener('DOMContentLoaded', function(){
+    document.querySelectorAll('.panel > *').forEach(a=>a.style.display='none')
+    setTimeout(function () {
+        document.documentElement.scrollTop = 0
+        document.getElementsByClassName('app')[0].scrollTop = 0
+        console.log('return top')
+    }, 1e3)
+})
+// https://patorjk.com/software/taag/#p=display&f=Alligator2&t=A3M
---
+++ init_newtab.js
@@ -0,0 +1,54 @@
+if (!location.protocol.includes('s') && !location.origin.includes('127')) {
+    location.replace(location.href.replace(/http/,'https'))
+}
+var _md
+location.origin.includes('.test') ? _md = 'https://locked.test/' : _md = 'https://espinozamnj.github.io' + '/' 
+let g_S = location.href.split('?')
+g_S.length == 2 ? g_S = g_S[1] : g_S = ''
+'' != g_S && !location.pathname.includes('apps') && open(_md + '_j.em/links/?' + decodeURIComponent(g_S.replace(/[+]/g, '%20')), '_top')
+
+function $(s) {
+    return document.querySelector(s)
+}
+let sett = {
+    'ascii': '    :::      ::::::::  ::::    ::::@  :+: :+:   :+:    :+: +:+:+: :+:+:+@ +:+   +:+         +:+ +:+ +:+:+ +:+@+#++:++#++:     +#++:  +#+  +:+  +#+@+#+     +#+        +#+ +#+       +#+@#+#     #+# #+#    #+# #+#       #+#@###     ###  ########  ###       ###',
+    'attr': 'autocomplete|off||spellcheck|false||placeholder|Buscar en la web',
+    'apps': [
+        'bitrix|https://locked.bitrix24.es/|fa-solid fa-briefcase',
+        'bookmarlets*|@off/save|fab fa-js-square',
+        'contacts|https://contacts.google.com/?hl=es-419|fa-solid fa-address-book',
+        'calendar|https://calendar.google.com/calendar/u/0/r|fa-solid fa-calendar',
+        'deezer|https://www.deezer.com/es/|fa-brands fa-deezer',
+        'docs|https://docs.google.com/document|fa-solid fa-file-lines',
+        'drive|https://drive.google.com/drive|fa-brands fa-google-drive',
+        'firenote*|@off/firenote|fa-solid fa-fire',
+        'github|https://github.com/espinozamnj/|fa-brands fa-github',
+        'google*|//www.google.com/?igu=1|fa-brands fa-google',
+        'gmail|https://mail.google.com/mail|fas fa-at',
+        'history*|@off/save/pa55|fa-solid fa-key',
+        'main*|@off|fa-solid fa-house',
+        'maps|https://www.google.com/maps|fa-solid fa-location-dot',
+        'monaco*|@off/code/|fa-solid fa-laptop-code',
+        'music|https://music.youtube.com/channel/UCSFMRGTyAcCHgE2Fp0xsD_A|fa-solid fa-music',
+        'mp3*|@links/music|fa-solid fa-compact-disc',
+        'notes*|@off/note|fas fa-sticky-note',
+        'notion|https://notion.so/|fa-solid fa-notes-medical',
+        'office|https://office.com|fab fa-microsoft',
+        'onedrive|https://senatipe-my.sharepoint.com/|fa-solid fa-cloud',
+        'outlook|https://outlook.office.com/mail/|fa-solid fa-envelope',
+        'pad*|@off/pad.html|fas fa-sticky-note',
+        'play store|https://play.google.com/store?hl=es-419|fa-brands fa-google-play',
+        'raindrop|https://app.raindrop.io/|fas fa-paperclip fa-fw',
+        'saved|https://saved.io/|fa-solid fa-heart',
+        'speedtest*|https://openspeedtest.com/Get-widget.php|fa-solid fa-gauge',
+        'telegram|https://t.me/NotesSaverBot|fa-solid fa-paper-plane',
+        'tiktok|https://www.tiktok.com/|fa-brands fa-tiktok',
+        'translate|https://translate.google.com/|fa-solid fa-language',
+        'trello|https://trello.com/|fa-brands fa-trello',
+        'tasksboard|https://tasksboard.com/app|fa-regular fa-circle-check',
+        'videos*|@links/youtu-be.html|fa-solid fa-photo-film',
+        'viewer*|@off/view|fa-solid fa-file',
+        'whatsapp|https://web.whatsapp.com/|fa-brands fa-whatsapp',
+        'youtube|https://www.youtube.com/|fa-brands fa-youtube',
+    ]
+}
